84fa0a42b088c6dcee93cdcd17c5a2c657a087e9
aMc-69 <CT.AMC.69@gmail.com>
2020-10-13 19:31:48 +0200

    Hello branch made as master

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("still awesome Hello");
+// added one more line after making hello branch as master
 
     }
 }
