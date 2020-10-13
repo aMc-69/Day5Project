df9dce10632ef8d2adb3314e7e476dd43b6df787
aMc-69 <CT.AMC.69@gmail.com>
2020-10-13 18:55:00 +0200

    Feature1 branch created 1 line added to Hello

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("still awesome Hello");
 
+// I added this line  after feature1 branch creation
+
     }
 }
